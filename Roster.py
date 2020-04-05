54840f87c69899688b7102894075c2135664bf3f
George Velonis <gvelonis@gmail.com>
2020-04-04 20:57:05 -0400

    Implemented base node and linkedlist classes, and player and roster child classes

--- Roster.py
+++ Roster.py
@@ -5,79 +5,161 @@
 #           April 4, 2020         #
 ###################################
 
-# initialize the dictionary that will hold the players and their ratings
-# the key is the players jersey number, the value is the rating
-roster = {}
 
-# program requires initial input of 5 players
-# for loop will repeat 5 times
-# jersey_num is better than just num, player_rating is better than just rating
-# because 'num' you don't know what it is for, with 'rating' you don't know what is being rated
-for i in range(5):
-    jersey_num = int(input('Enter player %d\'s jersey number: \n' % (i + 1)))
-    player_rating = int(input('Enter player %d\'s rating: \n\n' % (i + 1)))
-    roster[jersey_num] = player_rating
+# define class for each linked list node
+class Node:
+    def __init__(self):
+        self.prev_node = None
+        self.next_node = None
 
-# roster is a dict with is inherently unordered. we can sort the keys and store them in a list
-# now the for loop will iterate through the ordered list of dict keys
-keys_sorted = sorted(roster.keys())
-print('ROSTER')
-for key in keys_sorted:
-    print('Jersey number: %d, Rating: %d' % (key, roster[key]))
 
-# this is the main while loop, prints the menu of commands then waits for user input
-# repeats after each command until the user quits
-user_cmd = ''
-while user_cmd != 'q':
-    print('\nMENU')
-    print('a - Add player')
-    print('d - Remove player')
-    print('u - Update player rating')
-    print('r - Output players above a rating')
-    print('o - Output roster')
-    print('q - Quit')
-    print()
+# define node child class to represent each player
+class Player(Node):
+    def __init__(self, name, position, jersey_num, player_rating):
+        Node.__init__(self)
+        self.name = name
+        self.position = position
+        self.jersey_num = jersey_num
+        self.player_rating = player_rating
 
-    user_cmd = input('Choose an option: \n')
+    def __str__(self):
+        string = 'Name: %s, Position: %s, Number: %d, Rating: %d' % (
+            self.name, self.position, self.jersey_num, self.player_rating)
+        return string
 
-    # uses the same logic as printing the roster at the beginning
-    # code could be improved by moving it to a method and calling the method in both places
-    # output roster
-    if user_cmd == 'o':
-        print('\nROSTER')
-        keys_sorted = sorted(roster.keys())
-        for key in keys_sorted:
-            print('Jersey number: %s, Rating: %d' % (key, roster[key]))
 
-    # add player to roster
-    elif user_cmd == 'a':
-        jersey_num = int(input('Enter new player\'s jersey number: \n'))
-        player_rating = int(input('Enter new player\'s rating: \n\n'))
-        roster[jersey_num] = player_rating
+# define the class for the linked list
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-    # remove player from roster
-    elif user_cmd == 'd':
-        jersey_num = int(input('Enter a jersey number: '))
-        del roster[jersey_num]
+    def add_node(self, node):
+        # if head is None, list is empty and node should be both head and tail
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+        # otherwise, set current tail's next to the new node, the new node's previous to current tail
+        # then update current tail to the new node
+        self.tail.next_node = node
+        node.prev_node = self.tail
+        self.tail = node
 
-    # update player rating
-    # exactly the same code as add player. making it a different option is better for users
-    # it allows for more clear verbiage and lets the user feel more in control
-    elif user_cmd == 'u':
-        jersey_num = int(input('Enter a jersey number: '))
-        player_rating = int(input('Enter a new rating for player: '))
-        roster[jersey_num] = player_rating
+    def __iter__(self):
+        cursor = self.head
+        while cursor is not None:
+            node = cursor
+            cursor = cursor.next_node
+            yield node
 
-    # prints all players above a certain rating
-    # does not print players in any order. could be refactored to iterate over a sorted list of keys
-    elif user_cmd == 'r':
-        rating_threshold = int(input('Enter a rating: '))
-        print('\nABOVE', rating_threshold)
-        for key in roster:
-            if roster[key] > rating_threshold:
-                print('Jersey number: %s, Rating: %d' % (key, roster[key]))
 
-# best practices used include proper use of white space between logical sections of code
-# code comments explaining why the decisions were made to use data types
-# comments explaining algorithms used
-# variable names are distinct and descriptive. python practice is to separate words in variable with _
+# roster class
+class Roster(LinkedList):
+    def __init__(self):
+        LinkedList.__init__(self)
+
+    def add_player(self, new_player):
+        LinkedList.add_node(self, new_player)
+
+    def add_interactive(self):
+        name = input('Enter player\'s name: ')
+        position = input('Enter player\'s position: ')
+        jersey_num = int(input('Enter player\'s jersey number: '))
+        player_rating = int(input('Enter player\'s rating: '))
+        new_player = Player(name, position, jersey_num, player_rating)
+        self.add_player(new_player)
+
+    def remove_player(self, jersey_num):
+        for cursor in self:
+            if cursor.jersey_num == jersey_num:
+                cursor.prev_node.next_node = cursor.next_node
+                cursor.next_node.prev_node = cursor.prev_node
+
+    def update_rating(self, jersey_num, rating):
+        for cursor in self:
+            if cursor.jersey_num == jersey_num:
+                cursor.player_rating = rating
+
+    def print(self, max_rating=100, min_rating=0):
+        print('ROSTER:')
+        for cursor in self:
+            if min_rating <= cursor.player_rating <= max_rating:
+                print(cursor)
+
+    # TODO: implement sorting functions
+    def sort_alpha(self):
+        pass
+
+    def sort_position(self):
+        pass
+
+    def sort_jersey(self):
+        pass
+
+    def sort_rating(self):
+        pass
+
+
+if __name__ == '__main__':
+    # initialize roster LL
+    roster = Roster()
+
+    # program requires initial input of 5 players
+    # statically define initial 5 for testing
+    player = Player('Bergie', 'C', 33, 99)
+    roster.add_node(player)
+    player = Player('Marchand', 'W', 26, 92)
+    roster.add_node(player)
+    player = Player('Warren', 'W', 18, 78)
+    roster.add_node(player)
+    player = Player('Chara', 'D', 72, 95)
+    roster.add_node(player)
+    player = Player('McDonnel', 'D', 88, 78)
+    roster.add_node(player)
+
+    # iterate through adding players to roster
+    # for i in range(5):
+    #     roster.add_interactive()
+
+    roster.print()
+
+    # this is the main while loop, prints the menu of commands then waits for user input
+    # repeats after each command until the user quits
+    user_cmd = ''
+    while user_cmd != 'q':
+        print('\nMENU')
+        print('a - Add player')
+        print('d - Remove player')
+        print('u - Update player rating')
+        print('r - Output players above a rating')
+        print('o - Output roster')
+        print('q - Quit')
+        print()
+
+        user_cmd = input('Choose an option: ')
+
+        # print roster
+        if user_cmd == 'o':
+            roster.print()
+
+        # add player to roster
+        elif user_cmd == 'a':
+            roster.add_interactive()
+
+        # remove player from roster
+        elif user_cmd == 'd':
+            player_num = int(input('Enter a jersey number: '))
+            roster.remove_player(player_num)
+
+        # update player rating
+        elif user_cmd == 'u':
+            player_num = int(input('Entery a jersey number: '))
+            new_rating = int(input('Enter new player rating: '))
+            roster.update_rating(player_num, new_rating)
+
+        # prints all players above a certain rating
+        # does not print players in any order. could be refactored to iterate over a sorted list of keys
+        elif user_cmd == 'r':
+            rating_threshold = int(input('Enter a rating: '))
+            roster.print(100, rating_threshold)
--- doc/README.txt
+++ doc/README.txt
@@ -10,4 +10,5 @@ Run MarketAPI.py to start ReST API, see Sample_API_calls.txt for examples
 
 Run MarketCLI.py to interact with database through the command line
 
-GV-7-1.py is the original artifact, included for reference
+GV-7-1.py is the original design/database artifact, included for reference
+GV-CS-200-Final3.py is the original algorithms/data structures artifact, included for reference
